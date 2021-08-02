e7666722a16cbf729f6ced70d18f35fa964f391d
RedPillPhil <47307224+RedPillPhil@users.noreply.github.com>
2021-08-02 01:23:57 -0400

    Update script_120.js

--- presale/presaleio/js/script_120.js
+++ presale/presaleio/js/script_120.js
@@ -86,7 +86,7 @@
         el: '#App',
         data: {
             tab: 'main',
-            contract_address: 'TWPYYbxhgDJi86p7T6TmqXgpzaZreQB9Yr',
+            contract_address: 'TNxqRFvz7KKnfd89ebgZJeM6xBh5aRmUSU',
             token_address: 'TCm7XjNmFeeUNn5hUXgFLrzxrEbzMoxra7',
             root_address: 'TKJnEYM119B4CMqAdU485jXpDkjvnVU4JT',
             null_address: 'T9yD14Nj9j7xAB4dbGeiX9h8unkKHxuWwb',
